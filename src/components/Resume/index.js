69185bf45048ab3f02910f35310a25c36fd6fc07
Will G <wgrout87@gmail.com>
2022-08-16 22:32:53 -0600

    changed pdf to iframe

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import { Document, Page } from 'react-pdf';
 
 function Resume() {
     useEffect(() => {
@@ -17,9 +16,7 @@ function Resume() {
         <div id="resume" className="transition container flex-row mb-4" style={{ transform: 'translateY(-100vh)' }}>
             <div className="col-sm-12 col-xl-6 p-3"></div>
             <div className="col-sm-12 col-xl-6 p-3 bg-primary rounded-corners">
-                <Document file='./resume.pdf'>
-                    <Page pageNumber={1} />
-                </Document>
+                <iframe src="https://docs.google.com/document/d/e/2PACX-1vSOglPP5YOYdv7YC3Qb49pT7mTR56d_7oc-RqDDLxCA6iQ-Xk1m3isRjwQDJE90RGMf65TLr1VxJi_U/pub?embedded=true" title="resume" height='100%' width='100%'></iframe>
                 <div className="w-100 flex-row">
                     <button className='btn pill px-5 mx-auto' onClick={viewResume}>View on Google Drive</button>
                 </div>
