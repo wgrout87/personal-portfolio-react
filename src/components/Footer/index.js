4e0d68ff7dd29ef249365ca53ce1a3f5bc560449
Will G <wgrout87@gmail.com>
2022-08-20 17:45:36 -0600

    modified footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaLinkedin, FaGithub, FaStackOverflow, FaEnvelope } from 'react-icons/fa';
+import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
 
 function Footer() {
     function toggleNightMode(e) {
@@ -19,10 +19,6 @@ function Footer() {
                 <a href="https://www.linkedin.com/in/william-grout-49945b232/" target="_blank" rel="noreferrer" className="tooltip">
                     <FaLinkedin />
                     <span className="tooltiptext">LinkedIn</span>
-                </a>
-                <a href="https://stackoverflow.com/users/18238467/william-grout" target="_blank" rel="noreferrer" className="tooltip">
-                    <FaStackOverflow />
-                    <span className="tooltiptext">Stack Overflow</span>
                 </a>
                 <a href="mailto: wgrout87@gmail.com" className="tooltip">
                     <FaEnvelope />
