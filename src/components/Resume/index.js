59f1a05c6fc69106e2dce72c750c6a07403075bf
Will G <wgrout87@gmail.com>
2022-08-19 22:40:50 -0600

    changed resume button to a

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -15,17 +15,13 @@ function Resume() {
         document.getElementById('me').style.backgroundPosition = '0% 85%';
     }, []);
 
-    function viewResume() {
-        window.location.replace('https://docs.google.com/document/d/e/2PACX-1vSOglPP5YOYdv7YC3Qb49pT7mTR56d_7oc-RqDDLxCA6iQ-Xk1m3isRjwQDJE90RGMf65TLr1VxJi_U/pub');
-    }
-
     return (
         <div id="resume" className="transition container flex-row mb-4" style={{ transform: 'translateY(-100vh)' }}>
             <div className="col-sm-12 col-xl-6 p-3"></div>
             <div className="col-sm-12 col-xl-6 p-3 bg-primary rounded-corners">
                 <iframe className="rounded-corners" src="https://docs.google.com/document/d/e/2PACX-1vSOglPP5YOYdv7YC3Qb49pT7mTR56d_7oc-RqDDLxCA6iQ-Xk1m3isRjwQDJE90RGMf65TLr1VxJi_U/pub?embedded=true" title="resume" height='100%' width='100%'></iframe>
                 <div className="w-100 flex-row">
-                    <button className='btn pill px-5 mx-auto' onClick={viewResume}>View on Google Drive</button>
+                    <a className='btn pill px-5 mx-auto' href="https://docs.google.com/document/d/e/2PACX-1vSOglPP5YOYdv7YC3Qb49pT7mTR56d_7oc-RqDDLxCA6iQ-Xk1m3isRjwQDJE90RGMf65TLr1VxJi_U/pub" target="_blank" rel="noreferrer">View on Google Drive</a>
                 </div>
             </div>
         </div>
