6159e1fe6d467ad91a065f9b099562463f78273a
WGrout <wgrout87@gmail.com>
2024-04-15 19:13:40 -0600

    altered about me text

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -20,10 +20,10 @@ function About() {
             <div className="col-11 col-md-11 col-lg-6 p-3 bg-primary mx-auto rounded-corners mb-4">
                 <h2 className="speech-bubble-text bg-light rounded-corners p-2 mb-3">About Me:</h2>
                 <p className="speech-bubble-text about-me bg-light rounded-corners p-2 mb-0 mt-2">
-                    My name is William Grout. I've been a professional pizza cook for seven years with Fiore Pizza, but am currently transitioning into full stack web development. As I rose to leadership positions at Fiore, I was entrusted with teaching new employees the skills they needed to not only succeed but thrive in their roles.
+                    My name is William Grout and I'm working toward a career change to web development. I worked for eight years in the restaurant industry, but have recently been establishing my corporate office bona fides at HealthEquity.
                     <br />
                     <br />
-                    I am a recent graduate of the University of Utah's Coding Bootcamp, where I have gained both front and backend web development skills. I've recently gained the hard skills necessary to enter the workplace as a full stack developer, but I've been cultivating the soft skills necessary to integrate into a team for years.
+                    I am a graduate of the University of Utah's Coding Bootcamp, where I have gained both front and backend web development skills. I'm hoping to leverage the attitude of hard work and diligence I've developed in my previous roles to demonstrate value with my new coding skills.
                 </p>
             </div>
         </div>
