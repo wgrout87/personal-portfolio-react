4c26ba2e06b272ebddf1ff67c48237c19e99270c
WGrout <wgrout87@gmail.com>
2024-04-15 19:22:11 -0600

    removed broken projects

--- src/components/PortfolioPiece/projects.js
+++ src/components/PortfolioPiece/projects.js
@@ -1,13 +1,13 @@
 var projects = [
-    {
-        title: "6 Degrees of Kevin Bacon",
-        id: "6degrees",
-        url: "https://dawnwogerman.github.io/6degreesofkevinbacon/",
-        github: "https://github.com/DawnWogerman/6degreesofkevinbacon",
-        img: "./assets/images/6_degrees.png",
-        summary: "Application allowing the user to play the game Six Degrees of Kevin Bacon",
-        technologies: "HTML, CSS, Javascript, Jquery, Bulma, API"
-    },
+    // {
+    //     title: "6 Degrees of Kevin Bacon",
+    //     id: "6degrees",
+    //     url: "https://dawnwogerman.github.io/6degreesofkevinbacon/",
+    //     github: "https://github.com/DawnWogerman/6degreesofkevinbacon",
+    //     img: "./assets/images/6_degrees.png",
+    //     summary: "Application allowing the user to play the game Six Degrees of Kevin Bacon",
+    //     technologies: "HTML, CSS, Javascript, Jquery, Bulma, API"
+    // },
     {
         title: "A Hero's Outline",
         id: "herosOutline",
@@ -35,24 +35,24 @@ var projects = [
         summary: "A timed quiz with questions related to web development",
         technologies: "HTML, CSS, Javascript, React"
     },
-    {
-        title: "Budget Tracker",
-        id: "budgetTracker",
-        url: "https://fathomless-springs-34063.herokuapp.com/",
-        github: "https://github.com/wgrout87/budget-tracker",
-        img: "./assets/images/budget_tracker.png",
-        summary: "A simple budget tracking application",
-        technologies: "PWA, MongoDB, IndexedDB"
-    },
-    {
-        title: "Book Search",
-        id: "bookSearch",
-        url: "https://evening-river-75441.herokuapp.com/",
-        github: "https://github.com/wgrout87/book-search",
-        img: "./assets/images/savedBooks.png",
-        summary: "A search engine allowing the user to search for and save books",
-        technologies: "GraphQL, Apollo Server"
-    }
+    // {
+    //     title: "Budget Tracker",
+    //     id: "budgetTracker",
+    //     url: "https://fathomless-springs-34063.herokuapp.com/",
+    //     github: "https://github.com/wgrout87/budget-tracker",
+    //     img: "./assets/images/budget_tracker.png",
+    //     summary: "A simple budget tracking application",
+    //     technologies: "PWA, MongoDB, IndexedDB"
+    // },
+    // {
+    //     title: "Book Search",
+    //     id: "bookSearch",
+    //     url: "https://evening-river-75441.herokuapp.com/",
+    //     github: "https://github.com/wgrout87/book-search",
+    //     img: "./assets/images/savedBooks.png",
+    //     summary: "A search engine allowing the user to search for and save books",
+    //     technologies: "GraphQL, Apollo Server"
+    // }
 ]
 
 export default projects;
